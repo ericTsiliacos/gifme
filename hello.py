2655190a1fcb74e220a5251dc2a2dff7dee564ad
Elana Koren and Eric tsiliacos <pair+ekoren+etsiliacos@pivotal.io>
2015-11-10 16:11:40 -0800

    Use requests for http requests and put dependecies in requirements.txt

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import urllib
+import requests
 import json
 
 app = Flask(__name__)
@@ -8,9 +8,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     search = request.args.get('search')
-    api_url = urllib.urlopen("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=%s" % search)
-    data = json.loads(api_url.read())
+    response = requests.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=%s" % search)
+    data = json.loads(response.content)
     gify_url = data['data']['fixed_width_small_url']
+
     return render_template('layout.html', gify_url=gify_url)
 
 if __name__ == "__main__":
