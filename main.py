36298d3e57b4cb3810a646133d03b982bbf3a948
Elana Koren and Eric tsiliacos <pair+ekoren+etsiliacos@pivotal.io>
2015-11-10 17:32:50 -0800

    move search behavior to new endpoint /search

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    search = request.args.get('search')
+    return ""
+
+@app.route("/search")
+def search():
+    search = request.args.get('term')
     gif_url = url_builders.build_gif_url(search)
     response = requests.get(gif_url)
     data = json.loads(response.content)
     gify_url = data['data']['fixed_height_downsampled_url']
 
     return render_template('layout.html', gify_url=gify_url)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
