374d72e74ee99cecd8667bbd76445ee86c8601f0
Elana Koren and Eric tsiliacos <pair+ekoren+etsiliacos@pivotal.io>
2015-11-13 10:49:03 -0800

    return json for the search endpoint

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
+from flask.ext.cors import CORS
+from flask.json import jsonify
 import requests
 import json
 import url_builders
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
 def index():
     return ""
 
-@app.route("/search")
+@app.route("/search.json")
 def search():
-    search = request.args.get('term')
-    gif_url = url_builders.build_gif_url(search)
+    search_term = request.args.get('term')
+    gif_url = url_builders.build_gif_url(search_term)
     response = requests.get(gif_url)
     data = json.loads(response.content)
     gify_url = data['data']['fixed_height_downsampled_url']
+    json_url = {"url": gify_url}
 
-    return render_template('layout.html', gify_url=gify_url)
+    return jsonify(json_url)
 
 if __name__ == "__main__":
     app.run(debug=True)
